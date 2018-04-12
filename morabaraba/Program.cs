20920e874f89d2c2c4bdaf5a8d448e84f5da9a97
DumieSolani <g16s2145@campus.ru.ac.za>
2018-04-12 15:37:57 +0200

    Interfaces

--- morabaraba/Program.cs
+++ morabaraba/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace morabaraba
 {
@@ -8,5 +9,44 @@ namespace morabaraba
         {
             Console.WriteLine("Hello World!");
         }
+    }
+    public interface IBoard
+    {
+        ICow Occupant(string position);
+        IEnumerable<string> Cows(Cow cow); // stores the cows 
+        void PlaceCow(ICow cow, string position); // places the cow on the board
+        void RemoveCapturedCow(); // removes the shot cow
+        void MillFormed(ICow cow);
+        void CheckPreviousMill(IMills Mill);
+    }
+    public enum Cow { X, O } // either X cow or O cow
+    public enum Status { Captured, Active, NotPlaced }
+
+    public interface IMills
+    {
+    }
+
+    public interface ICow
+    {
+        IEnumerable<string> NormalMoves(IBoard board);
+        IEnumerable<string> MillForms(IBoard board);
+        Status Status { get; }
+        Cow Cow { get; }
+        string position { get; }
+        void Move(string destination);
+        void Shoot(string destination);
+        void Fly(string destination);
+        void Place(string destination);
+    }
+    public interface IPlayer
+    {
+        string Name { get; }
+        (int,int) getMove();
+    }
+    public interface IReferee
+    {
+        IPlayer Winner();
+        bool IsDraw();
+        void Play();
     }
 }
