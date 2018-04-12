a19bcdb063b4260ad6f7ee6a8097aaff55c47b14
DumieSolani <g16s2145@campus.ru.ac.za>
2018-04-12 16:01:51 +0200

    Class Stubs

--- morabaraba/Program.cs
+++ morabaraba/Program.cs
@@ -24,6 +24,8 @@ namespace morabaraba
 
     public interface IMills
     {
+        IEnumerable<string> Mills(IBoard board);
+        bool MillFormed(IBoard board);
     }
 
     public interface ICow
@@ -48,5 +50,114 @@ namespace morabaraba
         IPlayer Winner();
         bool IsDraw();
         void Play();
+    }
+    public class illegalMoveException : ApplicationException { }
+    public class Board : IBoard
+    {
+        public void CheckPreviousMill(IMills Mill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Cows(Cow cow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MillFormed(ICow cow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICow Occupant(string position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceCow(ICow cow, string position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCapturedCow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Player : IPlayer
+    {
+        public string Name => throw new NotImplementedException();
+
+        public (int, int) getMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Cows : ICow
+    {
+        public Status Status => throw new NotImplementedException();
+
+        public string position => throw new NotImplementedException();
+
+        Cow ICow.Cow => throw new NotImplementedException();
+
+        public void Fly(string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> MillForms(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> NormalMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Place(string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot(string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Referee : IReferee
+    {
+        public bool IsDraw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayer Winner()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Mill : IMills
+    {
+        public bool MillFormed(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Mills(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
