bdd6c79b6e81214a79eb21c5dc43ea2b71c09707
DumieSolani <g16s2145@campus.ru.ac.za>
2018-04-12 14:35:29 +0200

    Initial Commit

---
+++ morabaraba.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace morabaraba.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ morabaraba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace morabaraba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
